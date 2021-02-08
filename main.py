9a774e1f0f9e49f1ac80646471d590389d386da1
Fdrv <oldgot@gmail.com>
2021-02-08 22:18:37 +0300

    Add redirect processing

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+from pathlib import Path
+
 import requests
 import urllib3
-from pathlib import Path
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 Path("./books").mkdir(parents=True, exist_ok=True)
-
 url = 'https://tululu.org/txt.php?id='
-
-
 id = 1
 
-while id < 11:
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
+while id <= 10:
     response = requests.get(url + str(id), verify=False)
     response.raise_for_status()
     print(f'Id = {id} has http-status : {response.status_code} and Content-Type : {response.headers["Content-Type"]}')
 
+    try:
+        check_for_redirect(response)
+    except requests.HTTPError:
+        print(f'EXEPTION at {id}')
+        id += 1
+        continue
+
     with open(f'./books//id{id}.txt', 'wb') as book:
         book.write(response.content)
-    id +=1
+
+    id += 1
