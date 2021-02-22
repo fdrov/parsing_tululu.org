d2543799c870b8985934299b9846dfb0ad6bc07d
Fdrv <oldgot@gmail.com>
2021-02-23 00:28:16 +0300

    download book covers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from pathlib import Path
 
 import pathvalidate
@@ -21,8 +22,18 @@ def get_book_name(book_id):
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     header_text = soup.find('body').find('h1').text
+    pic_path = soup.find('div', class_='bookimage').find('img')['src']
+    pic_url = urllib.parse.urljoin(url, pic_path)
     title, author = header_text.split('::')
-    return pathvalidate.sanitize_filename(title.strip())
+    return {'title': pathvalidate.sanitize_filename(title.strip()), 'pic_url': pic_url}
+
+
+def download_image(pic_url, folder='images/'):
+    response = requests.get(pic_url, verify=False)
+    Path(f'{folder}').mkdir(parents=True, exist_ok=True)
+    img_name = urllib.parse.unquote(urllib.parse.urlsplit(pic_url).path.split('/')[-1])
+    with open(f'{folder}{img_name}', 'wb') as img:
+        img.write(response.content)
 
 
 def download_txt(url, folder='books/'):
@@ -41,8 +52,11 @@ def download_txt(url, folder='books/'):
         pass
     else:
         Path(f'{folder}').mkdir(parents=True, exist_ok=True)
-        with open(f'{folder}{id}. {get_book_name(id)}.txt', 'wb') as book:
+        book_info = get_book_name(id)
+        download_image(book_info["pic_url"])
+        with open(f'{folder}{id}. {book_info["title"]}.txt', 'wb') as book:
             book.write(response.content)
+        print(f'Заголовок: {book_info["title"]}', book_info["pic_url"], sep='\n')
 
 
 while id <= 10:
